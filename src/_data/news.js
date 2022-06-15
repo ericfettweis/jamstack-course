99157f98097fc67ed92192013d010c142516dc1b
Eric Fettweis <eric@fettweis.fr>
2022-06-15 10:48:29 +0200

    redirects to country specific

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,24 @@
 const axios = require("axios");
+const countries = require('./countries.json');
 require("dotenv").config();
 
-module.exports = async function getNews(){
+async function getNews(country){
     const apiKey = process.env.API_KEY;//"";
-    const country='fr';
     const url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`
     try{
         const response = await axios.get(url);
-        return response.data;
+        return {
+            country: country,
+            articles: response.data.articles
+        };
     }catch(err){
         console.error(err);
     }
+}
+
+module.exports = async function (){
+    var newsPromises = countries.map(getNews);
+    return Promise.all(newsPromises).then(newsObjects=>{
+        return [].concat.apply([],newsObjects);
+    });
 };
